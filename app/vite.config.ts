072837f4e995c03472a4b41cbdafae6b84445617
sandgupt <sandgupt@adobe.com>
2026-03-23 00:45:38 -0600

    update for github pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jagdamba_automobiles/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
 })
